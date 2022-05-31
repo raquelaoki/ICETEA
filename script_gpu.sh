aece62cf22b41d1a82b2316a1f8a0dfce0d33045
Raquel Aoki <raquel.aoki@gmail.com>
2022-05-30 21:02:48 -0700

    gc

---
+++ script_gpu.sh
@@ -0,0 +1,12 @@
+sudo nvidia-smi
+
+sudo apt-get update
+sudo apt-get install wget
+sudo apt install python3 python3-dev python3-venv
+sudo apt-get install python3-pip
+sudo apt install git
+sudo apt-get install libgl1
+sudo apt-get install libxml2
+
+
+
